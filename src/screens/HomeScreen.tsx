35263de0573e40508a39357c5c4f163205ced09d
KarenMinasyan <kminasyantricking@gmail.com>
2023-04-26 17:56:14 +0400

    refactor home styles

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Dimensions, Image, SafeAreaView, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Dimensions, Image, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { ADVANTURES, CATEGORIES, COLORS, SPACING } from "../helpers/constants";
 
@@ -9,31 +9,27 @@ const HomeScreen = () => {
   const [activeCategory, setActiveCategory] = useState(0);
   return (
     <SafeAreaView>
-      <View style={{ padding: SPACING * 2 }}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-          <Text style={{ fontSize: SPACING * 3, fontWeight: "bold", color: COLORS.dark }}>Discover</Text>
-          <Image style={{ height: SPACING * 5, width: SPACING * 5, borderRadius: SPACING * 5 }}
-                 source={require("../assets/images/Avatar.png")} />
+      <View style={styles.container}>
+        <View style={styles.discover}>
+          <Text style={styles.discoverTitle}>Discover</Text>
+          <Image style={styles.account} source={require("../assets/images/Avatar.png")} />
         </View>
-        <ScrollView style={{ marginVertical: SPACING * 2 }} horizontal>
+        <ScrollView style={styles.categories} horizontal>
           {
             CATEGORIES.map((category, index) => (
               <TouchableOpacity
-                style={{ marginRight: SPACING }}
+                style={styles.category}
                 key={category.id}
                 onPress={() => setActiveCategory(index)}
               >
-                <Text style={[{
-                  fontSize: SPACING * 2,
-                  color: COLORS.dark
-                }, activeCategory === index && { color: COLORS.primary }]}>
+                <Text style={[styles.categoryTitle, activeCategory === index && { color: COLORS.primary }]}>
                   {category.title}
                 </Text>
               </TouchableOpacity>
             ))
           }
         </ScrollView>
-        <Text style={{ fontSize: SPACING * 1.7, color: COLORS.dark }}>
+        <Text style={styles.activities}>
           {CATEGORIES[activeCategory].tours.length + " "}
           {CATEGORIES[activeCategory].title}
         </Text>
@@ -43,67 +39,38 @@ const HomeScreen = () => {
           snapToInterval={width * 0.7}
           decelerationRate="fast"
           pagingEnabled
-          style={{ marginVertical: SPACING * 2 }}
+          style={styles.tours}
         >
           {CATEGORIES[activeCategory].tours.map((tour, index) => (
-            <TouchableOpacity style={{
-              width: width * 0.7,
-              height: width * 0.9,
-              overflow: "hidden",
-              borderRadius: SPACING * 2,
-              marginRight: SPACING * 2
-            }} key={index}>
-              <View style={{
-                position: "absolute",
-                zIndex: 1,
-                height: "100%",
-                width: "100%",
-                backgroundColor: COLORS.transparent,
-                justifyContent: "space-between",
-                padding: SPACING
-              }}>
-                <TouchableOpacity style={{
-                  alignSelf: "flex-end",
-                  padding: SPACING / 2,
-                  backgroundColor: COLORS.white,
-                  borderRadius: SPACING * 5,
-                  justifyContent: "center",
-                  alignItems: "center"
-                }}>
+            <TouchableOpacity style={styles.card} key={index}>
+              <View style={styles.tour}>
+                <TouchableOpacity style={styles.favorite}>
                   <Icon name="heart-outline" size={SPACING * 4} color={COLORS.primary} />
                 </TouchableOpacity>
-                <Text style={{
-                  fontSize: SPACING * 2,
-                  color: COLORS.white,
-                  fontWeight: "700",
-                  marginLeft: SPACING
-                }}>{tour.title}</Text>
+                <Text style={styles.tourTitle}>{tour.title}</Text>
               </View>
-              <Image source={tour.image} style={{ width: "100%", height: "100%" }} />
+              <Image source={tour.image} style={styles.tourPreview} />
             </TouchableOpacity>
           ))}
         </ScrollView>
-        <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-          <Text style={{ fontSize: SPACING * 2, fontWeight: "bold", color: COLORS.dark }}>Feeling Adventures?</Text>
+        <View style={styles.options}>
+          <Text style={styles.feeling}>Feeling Adventures?</Text>
           <TouchableOpacity>
-            <Text style={{ fontSize: SPACING * 1.4, fontWeight: "500", color: COLORS.primary }}>Show all</Text>
+            <Text style={styles.all}>Show all</Text>
           </TouchableOpacity>
         </View>
         <ScrollView
           horizontal
           pagingEnabled
-          style={{ marginVertical: SPACING * 2 }}
+          style={styles.adventures}
           showsHorizontalScrollIndicator={false}
         >
           {ADVANTURES.map(adventure => (
-            <TouchableOpacity key={adventure.id}
-                              style={{ marginRight: SPACING * 3, padding: SPACING, alignItems: "center" }}>
-              <View style={{ width: SPACING * 3, height: SPACING * 3 }}>
-                <Image source={adventure.image} resizeMode="contain" style={{ width: "100%", height: "100%" }} />
+            <TouchableOpacity key={adventure.id} style={styles.adventure}>
+              <View style={styles.adventurePreview}>
+                <Image source={adventure.image} resizeMode="contain" style={styles.adventureImg} />
               </View>
-              <Text style={{ textTransform: "uppercase", fontSize: SPACING, marginTop: SPACING }}>
-                {adventure.title}
-              </Text>
+              <Text style={styles.adventureTitle}>{adventure.title}</Text>
             </TouchableOpacity>
           ))}
         </ScrollView>
@@ -113,3 +80,111 @@ const HomeScreen = () => {
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: SPACING * 2
+  },
+  discover: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  discoverTitle: {
+    fontSize: SPACING * 3,
+    fontWeight: "bold",
+    color: COLORS.dark
+  },
+  account: {
+    height: SPACING * 5,
+    width: SPACING * 5,
+    borderRadius: SPACING * 5
+  },
+  categories: {
+    marginVertical: SPACING * 2
+  },
+  category: {
+    marginRight: SPACING
+  },
+  categoryTitle: {
+    fontSize: SPACING * 2,
+    color: COLORS.dark
+  },
+  activities: {
+    fontSize: SPACING * 1.7,
+    color: COLORS.dark
+  },
+  tours: {
+    marginVertical: SPACING * 2
+  },
+  card: {
+    width: width * 0.7,
+    height: width * 0.9,
+    overflow: "hidden",
+    borderRadius: SPACING * 2,
+    marginRight: SPACING * 2
+  },
+  tour: {
+    position: "absolute",
+    zIndex: 1,
+    height: "100%",
+    width: "100%",
+    backgroundColor: COLORS.transparent,
+    justifyContent: "space-between",
+    padding: SPACING
+  },
+  favorite: {
+    alignSelf: "flex-end",
+    padding: SPACING / 2,
+    backgroundColor: COLORS.white,
+    borderRadius: SPACING * 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  tourTitle: {
+    fontSize: SPACING * 2,
+    color: COLORS.white,
+    fontWeight: "700",
+    marginLeft: SPACING
+  },
+  tourPreview: {
+    width: "100%",
+    height: "100%"
+  },
+  options: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  feeling: {
+    fontSize: SPACING * 2,
+    fontWeight: "bold",
+    color: COLORS.dark
+  },
+  all: {
+    fontSize: SPACING * 1.4,
+    fontWeight: "500",
+    color: COLORS.primary
+  },
+  adventures: {
+    marginVertical: SPACING * 2
+  },
+  adventure: {
+    marginRight: SPACING * 3,
+    padding: SPACING,
+    alignItems: "center"
+  },
+  adventurePreview: {
+    width: SPACING * 3,
+    height: SPACING * 3
+  },
+  adventureImg: {
+    width: "100%",
+    height: "100%"
+  },
+  adventureTitle: {
+    textTransform: "uppercase",
+    fontSize: SPACING,
+    marginTop: SPACING
+  }
+});
